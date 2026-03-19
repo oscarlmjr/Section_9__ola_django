d681597bc39984a41a1beff4c35fa4a98098b314
oscarlmjr <oscarlmjr@gmail.com>
2026-03-19 19:37:18 -0300

    secao_9 aula_445-447

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from typing import Any
 from blog.data import posts
-from django.http import HttpRequest
+from django.http import HttpRequest, Http404
 from django.shortcuts import render
 
 
@@ -27,7 +27,7 @@ def post(request: HttpRequest, post_id: int):
 			break
 
 	if found_post is None:
-		raise Exception('Post não existe.')
+		raise Http404('Post não existe.')
 	
 	context = {
 			'post': found_post,
